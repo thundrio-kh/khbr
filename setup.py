788c855acc0ba2d4db75a8e502849c55a07ea16c
Sam <12samn@gmail.com>
2021-05-02 12:35:58 -0700

    pip version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
 	name = 'khbr',
 	packages = find_packages(),
 	package_data={'': ['data/ai_mods/*', '*.json', '*.yaml']},
-	version = 0.52,
+	version = 0.61,
 	long_description = long_description,
 	long_description_content_type='text/markdown',
 	license = 'MIT',
