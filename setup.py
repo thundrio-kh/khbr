4ef6862bfdf92aafead5fec91fe20f529ae7e93d
thundrio <thundrio>
2021-05-08 10:37:12 -0700

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
 	name = 'khbr',
 	packages = find_packages(),
 	package_data={'': ['data/ai_mods/*', '*.json', '*.yaml']},
-	version = 0.8,
+	version = 0.81,
 	long_description = long_description,
 	long_description_content_type='text/markdown',
 	license = 'MIT',
