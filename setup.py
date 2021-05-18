a7af39e250c7feccf88cd10f22d297c8b7b86602
Sam <12samn@gmail.com>
2021-05-18 08:57:40 -0700

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
 	name = 'khbr',
 	packages = find_packages(),
 	package_data={'': ['data/ai_mods/*', '*.json', '*.yaml']},
-	version = 0.94,
+	version = 0.96,
 	long_description = long_description,
 	long_description_content_type='text/markdown',
 	license = 'MIT',
