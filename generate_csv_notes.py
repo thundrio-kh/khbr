b5f847f9bdbfd5a32571efda2ce9016838c023da
thundrio <thundrio>
2021-05-30 12:16:07 -0700

    nightmare notes

--- generate_csv_notes.py
+++ generate_csv_notes.py
@@ -21,17 +21,20 @@ exclusions_enemies = []
 kh2 = KingdomHearts2()
 
 bosses = kh2.get_bosses(usefilters=["boss"])
+boss_names = sorted(bosses)
+
+bosslines = []
 
 sources = []
-
-for source_boss_name in bosses:
+names = []
+for source_boss_name in boss_names:
     if source_boss_name in exclusions:
         continue
     source_boss = bosses[source_boss_name]
     if source_boss["parent"] != source_boss["name"]:
         continue
     source_boss_compatability = [source_boss_name]
-    for new_boss_name in bosses:
+    for new_boss_name in boss_names:
         if new_boss_name in exclusions:
             continue
         new_boss = bosses[new_boss_name]
@@ -44,12 +47,50 @@ for source_boss_name in bosses:
             compat = 'X'
         
         source_boss_compatability.append(compat)
+        names.append(new_boss_name)
     sources.append(source_boss_compatability)
 
-lines = [[""]+[s[0] for s in sources]]+sources
+
+bosslines.append([""]+sorted(set(names)))
+bosslines += sources
 
 with open("boss_compat.csv", "w") as f:
-    f.write("\n".join([",".join(s) for s in lines]))
+    f.write("\n".join([",".join(s) for s in bosslines]))
+
+bosslines_nightmare =[]
+
+sources_nightmare = []
+names_nightmare = []
+for source_boss_name in boss_names:
+    if source_boss_name in exclusions:
+        continue
+    source_boss = bosses[source_boss_name]
+    if source_boss["parent"] != source_boss["name"]:
+        continue
+    source_boss_compatability = [source_boss_name]
+    for new_boss_name in boss_names:
+        if new_boss_name in exclusions:
+            continue
+        new_boss = bosses[new_boss_name]
+        if not new_boss["isnightmare"]:
+            continue
+        if new_boss["parent"] != new_boss["name"]:
+            continue
+    
+        if new_boss["name"] in source_boss["available"]:
+            compat = 'O'
+        else:
+            compat = 'X'
+        
+        source_boss_compatability.append(compat)
+        names_nightmare.append(new_boss_name)
+    sources_nightmare.append(source_boss_compatability)
+
+bosslines_nightmare.append([""]+sorted(set(names_nightmare)))
+bosslines_nightmare += sources_nightmare
+
+with open("boss_compat_nightmare.csv", "w") as f:
+    f.write("\n".join([",".join(s) for s in bosslines_nightmare]))
 
 title_rows = []
 lines = []
@@ -69,3 +110,22 @@ for c in categories:
 
 with open("enemy_cats.csv", "w") as f:
     f.write("\n".join([",".join(s) for s in lines]))
+
+title_rows_nightmare = []
+lines_nightmare = []
+enemies = kh2.get_enemies()
+enemies_dict = {}
+for v in enemies:
+    k = v["name"]
+    enemies_dict[k] = v
+categories = kh2.categorize_enemies(enemies)
+for c in categories:
+    lines_nightmare.append([c])
+    title_rows_nightmare.append(len(lines_nightmare))
+    for e in sorted(categories[c]):
+        enemy = enemies_dict[e]
+        if enemy["name"] == enemy["parent"] and enemy["isnightmare"]:
+            lines_nightmare.append(sorted(enemy["children"]))
+
+with open("enemy_cats_nightmare.csv", "w") as f:
+    f.write("\n".join([",".join(s) for s in lines_nightmare]))
--- make_notes.py
+++ make_notes.py
@@ -66,6 +66,54 @@ for c in categories:
         if enemy["name"] == enemy["parent"]:
             lines.append(sorted(enemy["children"]))
 
+bosslines_nightmare =[]
+
+sources_nightmare = []
+names_nightmare = []
+for source_boss_name in boss_names:
+    if source_boss_name in exclusions:
+        continue
+    source_boss = bosses[source_boss_name]
+    if source_boss["parent"] != source_boss["name"]:
+        continue
+    source_boss_compatability = [source_boss_name]
+    for new_boss_name in boss_names:
+        if new_boss_name in exclusions:
+            continue
+        new_boss = bosses[new_boss_name]
+        if not new_boss["isnightmare"]:
+            continue
+        if new_boss["parent"] != new_boss["name"]:
+            continue
+    
+        if new_boss["name"] in source_boss["available"]:
+            compat = 'O'
+        else:
+            compat = 'X'
+        
+        source_boss_compatability.append(compat)
+        names_nightmare.append(new_boss_name)
+    sources_nightmare.append(source_boss_compatability)
+
+bosslines_nightmare.append([""]+sorted(set(names_nightmare)))
+bosslines_nightmare += sources_nightmare
+
+title_rows_nightmare = []
+lines_nightmare = []
+enemies = kh2.get_enemies()
+enemies_dict = {}
+for v in enemies:
+    k = v["name"]
+    enemies_dict[k] = v
+categories = kh2.categorize_enemies(enemies)
+for c in categories:
+    lines_nightmare.append([c])
+    title_rows_nightmare.append(len(lines_nightmare))
+    for e in sorted(categories[c]):
+        enemy = enemies_dict[e]
+        if enemy["name"] == enemy["parent"] and enemy["isnightmare"]:
+            lines_nightmare.append(sorted(enemy["children"]))
+
 
 print("Data made: {}".format(time.time()-start_time))
 import sys; sys.stdout.flush()
@@ -84,10 +132,13 @@ sys.stdout.flush()
 
 boss_sheet=sheet[2]
 
+
 red = (1, 0, 0, 0)
 green = (0.5, 1, 0, 0)
+
 for row_index in range(len(boss_lines)):
     row = boss_lines[row_index]
+    print(len(row))
     for col_index in range(len(row)):
         value = row[col_index]
         cell = boss_sheet.cell((row_index+1, col_index+1))
@@ -104,11 +155,43 @@ enemy_sheet = sheet[1]
 for l in range(len(lines)):
     enemy_sheet.update_row(l+1, lines[l])
 
+for i in title_rows:
+    enemy_sheet.cell((i, 1)).set_text_format('bold', True).set_text_format('fontSize', 20)
+
 print("Enemy rows updated: {}".format(time.time()-start_time))
 sys.stdout.flush()
 
+boss_sheet_nightmare=sheet[4]
+
+
+red = (1, 0, 0, 0)
+green = (0.5, 1, 0, 0)
+
+for row_index in range(len(bosslines_nightmare)):
+    row = bosslines_nightmare[row_index]
+    print(len(row))
+    for col_index in range(len(row)):
+        value = row[col_index]
+        cell = boss_sheet_nightmare.cell((row_index+1, col_index+1))
+        cell.value = value
+        if value in ['X', 'O']:
+            cell.color = green if value == 'O' else red
+        cell.update()
+
+
+print("Nightmare Boss rows updated: {}".format(time.time()-start_time))
+sys.stdout.flush()
+
+enemy_sheet_nightmare = sheet[3]
+
+for l in range(len(lines_nightmare)):
+    enemy_sheet_nightmare.update_row(l+1, lines_nightmare[l])
+
 for i in title_rows:
-    enemy_sheet.cell((i, 1)).set_text_format('bold', True).set_text_format('fontSize', 20)
+    enemy_sheet_nightmare.cell((i, 1)).set_text_format('bold', True).set_text_format('fontSize', 20)
+
+print("Nightmare Enemy rows updated: {}".format(time.time()-start_time))
+sys.stdout.flush()
 
 # gc.run_batch()
 
