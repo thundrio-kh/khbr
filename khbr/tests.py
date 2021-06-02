1944ac52398fef025e23ea957d64b54d388ac5c8
Sam <12samn@gmail.com>
2021-06-01 17:58:07 -0700

    some comments on tests

--- khbr/tests.py
+++ khbr/tests.py
@@ -62,22 +62,29 @@ class Tests(unittest.TestCase):
     def test_seedgen_enemy_one_to_one(self):
         options = {"enemy": "One to One"}
         randomization = self._generateSeed(options)
+        # Validate shadows outside tower are same as shadows outside enemy
 
     def test_seedgen_enemy_one_to_one_nightmare(self):
         options = {"enemy": "One to One", "nightmare_enemies": True}
         randomization = self._generateSeed(options)
+        #validate shadows are not found anywhere and shadows outside tower are same as star room shadows
 
     def test_seedgen_enemy_one_to_one_room(self):
         options = {"enemy": "One to One Per Room"}
         randomization = self._generateSeed(options)
+        # Validate all shadows outside tower are same
+        # but in star tower they are all same but different
 
     def test_seedgen_enemy_one_to_one_room_nightmare(self):
         options = {"enemy": "One to One Per Room", "nightmare_enemies": True}
         randomization = self._generateSeed(options)
+        # Validate no shadows exist
+        # but in star tower they are different than outside
 
     def test_seedgen_enemy_selected(self):
         options = {"enemy": "Selected Enemy", "selected_enemy": "Shadow WI"}
         randomization = self._generateSeed(options)
+        # Validate all enemies are shadow wi
 
     def test_seedgen_boss_one_to_one(self):
         options = {"boss": "One to One"}
@@ -89,35 +96,43 @@ class Tests(unittest.TestCase):
     def test_seedgen_boss_one_to_one_scaled(self):
         options = {"boss": "One to One", "scale_boss_stats": True}
         randomization = self._generateSeed(options)
+        # validate the scale map matches the randomization map
 
     def test_seedgen_boss_one_to_one_cups(self):
         options = {"boss": "One to One", "cups_bosses": True}
         randomization = self._generateSeed(options)
+        # validate cups bosses are present
 
     def test_seedgen_boss_one_to_one_datas(self):
         options = {"boss": "One to One", "data_bosses": True}
         randomization = self._generateSeed(options)
+        # validate datas are present
 
     def test_seedgen_boss_one_to_one_cups_datas(self):
         options = {"boss": "One to One", "cups_bosses": True, "data_bosses": True}
         randomization = self._generateSeed(options)
+        # validate cups and datas are both present
 
     def test_seedgen_boss_wild(self):
         options = {"boss": "Wild"}
         randomization = self._generateSeed(options)
         # validate cups/datas are off
+        # validate those like FX are randomized, but Jafar isn't
 
     def test_seedgen_boss_wild_scaled(self):
         options = {"boss": "Wild", "scale_boss_stats": True}
         randomization = self._generateSeed(options)
+        # validate scaling worked right
 
     def test_seedgen_boss_wild_cups(self):
         options = {"boss": "Wild", "cups_bosses": True}
         randomization = self._generateSeed(options)
+        # validate datas are present
 
     def test_seedgen_boss_wild_datas(self):
         options = {"boss": "Wild", "data_bosses": True}
         randomization = self._generateSeed(options)
+        # validate datas are present
 
     def test_seedgen_boss_wild_cups_datas(self):
         options = {"boss": "Wild", "cups_bosses": True, "data_bosses": True}
@@ -166,3 +181,5 @@ unittest.main()
 # ut.test_seedgen_proderror1()
 
 # memory expansion=true test for enemies, validate certain rooms were ignored/not ignored
+
+# I think I'm going to want a lot of "analyze randomization" functions so I can easily test lots of things in each test
