50dbf58251f9b69a24450dfd5d3b25328130f3b1
thundrio <12samn@gmail.com>
2021-08-11 21:38:26 -0700

    fix a couple paradox cup rounds

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
 	name = 'khbr',
 	packages = find_packages(),
 	package_data={'': ['data/ai_mods/*', '*.json', '*.yaml', 'data/*.yml', 'data/*.areadatascript']},
-	version = '1.2.0',
+	version = '1.2.1',
 	long_description = long_description,
 	long_description_content_type='text/markdown',
 	license = 'MIT',
