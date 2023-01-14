ad59e0dd4a4ef8ca26fd5215d3fa659f73672527
thundrio <12samn@gmail.com>
2023-01-14 13:15:17 -0800

    fix for karma rando

--- khbr/KH2/AiManager.py
+++ khbr/KH2/AiManager.py
@@ -13,9 +13,9 @@ class AiManager:
         if karma_param.search(self.script):
             self.script = karma_param.sub(lambda m: "pushImmf {}\n syscall 2, 76 ; trap_enemy_set_karma_limit ".format(str(available_values.pop())),  self.script)
             return
-        act_table_syscall = re.compile(r'pushImm .*\n.*; trap_act_table_add \(12 in, 0 out\)(\n)')
+        act_table_syscall = re.compile(r'\n.*; trap_act_table_add \(12 in, 0 out\)(\n)')
         if act_table_syscall.search(self.script):
-            self.script = act_table_syscall.sub(lambda m: "syscall 1, 6 ; trap_act_table_add (12 in, 0 out)\n pushFromFSp 0\n pushImmf {}\n syscall 2, 76 ; trap_enemy_set_karma_limit \n".format(str(available_values.pop())), self.script, count=1)
+            self.script = act_table_syscall.sub(lambda m: "\n syscall 1, 6 ; trap_act_table_add (12 in, 0 out)\n pushFromFSp 0\n pushImmf {}\n syscall 2, 76 ; trap_enemy_set_karma_limit \n".format(str(available_values.pop())), self.script, count=1)
         return
 
     def get_script(self):
