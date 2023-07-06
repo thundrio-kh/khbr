bca0267246c07228e68efd90c306abd1e38153a5
thundrio <12samn@gmail.com>
2023-07-05 22:07:03 -0700

    v4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
 	name = 'khbr',
 	packages = find_packages(),
 	include_package_data=True,
-	version = '3.1.1',
+	version = '4.0',
 	package_data={'': ['KH2/data/**/**']},
 	long_description = long_description,
 	long_description_content_type='text/markdown',
